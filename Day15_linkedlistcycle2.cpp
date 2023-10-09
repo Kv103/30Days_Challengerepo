d901297e939935eac596df5f537ace4702c5eeac
Kv103 <97123049+Kv103@users.noreply.github.com>
2023-10-09 21:45:19 +0530

    Add files via upload

---
+++ Day15_linkedlistcycle2.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        ListNode*slow= head;
+        ListNode*fast= head;
+
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow= slow->next;
+            fast= fast->next->next;
+
+            if(slow==fast)
+            {
+                while(head!=slow)
+                {
+                    head= head->next;
+                    slow=slow->next;
+                }
+
+                return slow;
+            }
+            // return slow;
+        }
+        return NULL;
+
+        // t-c-> O(n)
+        // s-c-> O(1)
+        
+    }
+};
